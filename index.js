dd3aa95a5480620a719e81c96123827423022c2d
mascotbeaver104 <49604587+mascotbeaver104@users.noreply.github.com>
2019-05-29 00:54:08 -0500

    First upload of website

---
+++ index.js
@@ -0,0 +1,23 @@
+import Game from "/src/game";
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d")
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+let lastTime = 0;
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+
+function gameLoop(timeStamp){
+    let deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+    gameLoop.update(deltaTime);
+    gameLoop.draw(ctx);
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
