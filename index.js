59d84bc3ce3e4a783326c7203fbe74a0106e0968
mascotbeaver104 <49604587+mascotbeaver104@users.noreply.github.com>
2019-06-01 18:21:38 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
-import Game from "game.js";
-let canvas = document.getElementById("gameScreen");
-let ctx = canvas.getContext("2d")
+var disp= '0';
+let inputArr = [];
+let accIn = true;
+let opIndex = -1;
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
-
-ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
-let lastTime = 0;
-let game = new Game(GAME_WIDTH, GAME_HEIGHT);
-
-function gameLoop(timeStamp){
-    let deltaTime = timeStamp - lastTime;
-    lastTime = timeStamp;
-    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
-    gameLoop.update(deltaTime);
-    gameLoop.draw(ctx);
-    requestAnimationFrame(gameLoop);
+let operators = {
+    '+': function(a, b){return Number(a)+Number(b)},
+    '-': function(a,b){return Number(a)-Number(b)},
+    '/': function(a,b){return Number(a)/Number(b)},
+    '*': function(a,b){return Number(a)*Number(b)}
 }
 
-requestAnimationFrame(gameLoop);
+function calculator(input){
+    if(/[0-9]|\./.test(input) && accIn){
+        inputArr.push(input);
+        disp = inputArr.join('');
+    }
+    if(/\+|\-|\*|\//.test(input)){
+        if(opIndex === -1 && inputArr.length > 0){
+        opIndex = inputArr.length;
+        inputArr.push(input);
+        disp=inputArr.join('');
+        accIn = true;
+        } else if(opIndex > -1 && inputArr.length-1 != opIndex){
+            disp = operators[inputArr[opIndex]](Number(inputArr.slice(0,opIndex).join('')), Number(inputArr.slice(opIndex+1).join('')));
+            inputArr = String(disp).split('');
+            opIndex = inputArr.length;
+            inputArr.push(input);
+            disp=inputArr.join('');
+;
+        }
+    }
+    if(/enter/.test(input)){
+        if(opIndex != 0 && inputArr.length-1 > opIndex){
+            accIn = false;
+            disp = operators[inputArr[opIndex]](Number(inputArr.slice(0,opIndex).join('')), Number(inputArr.slice(opIndex+1).join('')));
+            inputArr = String(disp).split('');
+            opIndex = -1;
+        }
+    }
+    if(/clear/.test(input)){
+        if(opIndex != 0 && inputArr.length-1 > opIndex){
+            accIn = false;
+            disp = operators[inputArr[opIndex]](Number(inputArr.slice(0,opIndex).join('')), Number(inputArr.slice(opIndex+1).join('')));
+            inputArr = String(disp).split('');
+            opIndex = -1;
+        }
+    }
+
+
+    document.getElementById('output').innerHTML = disp;
+}
+
+document.getElementById('one').addEventListener('click', function(){calculator(1)});
+document.getElementById('two').addEventListener('click', function(){calculator(2)});
+document.getElementById('three').addEventListener('click', function(){calculator(3)});
+document.getElementById('four').addEventListener('click', function(){calculator(4)});
+document.getElementById('five').addEventListener('click', function(){calculator(5)});
+document.getElementById('six').addEventListener('click', function(){calculator(6)});
+document.getElementById('seven').addEventListener('click', function(){calculator(7)});
+document.getElementById('eight').addEventListener('click', function(){calculator(8)});
+document.getElementById('nine').addEventListener('click', function(){calculator(9)});
+document.getElementById('zero').addEventListener('click', function(){calculator(0)});
+document.getElementById('add').addEventListener('click', function(){calculator('+')});
+document.getElementById('multiply').addEventListener('click', function(){calculator('*')});
+document.getElementById('divide').addEventListener('click', function(){calculator('/')});
+document.getElementById('subtract').addEventListener('click', function(){calculator('-')});
+document.getElementById('enter').addEventListener('click', function(){calculator('enter')});
+document.getElementById('decimal').addEventListener('click', function(){calculator('.')});
+document.getElementById('c').addEventListener('click', function(){calculator('clear')});
+document.getElementById('ac').addEventListener('click', function(){calculator('allClear')});
+document.getElementById('output').innerHTML = disp;
